5a0e9f909781932714385d6bddb34c2e2ce7aa37
Charel Samuel <charel.matulessy@aux.dkatalis.com>
2023-08-29 18:51:42 +0700

    reroute index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,11 +6,8 @@
 
   <!-- Page Heading -->
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
   </div>
-
-<button type="button" class="btn btn-success mb-3"><i class="fas fa-plus"> Add</i></button>
-
 
 </div>
 <!-- /.container-fluid -->
