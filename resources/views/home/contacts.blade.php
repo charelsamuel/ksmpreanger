bec58e2e0e18edc9031722cda43e05a882fe9c7b
Charel Samuel <charel.matulessy@aux.dkatalis.com>
2023-09-19 20:28:12 +0700

    remove map

--- resources/views/home/contacts.blade.php
+++ resources/views/home/contacts.blade.php
@@ -1,5 +1,5 @@
 <x-coffee>
-<!-- PAGE HEAD -->
+	<!-- PAGE HEAD -->
 	<section class="page-head">
 		<div class="container">
 			<div class="row">
@@ -8,43 +8,43 @@
 						<li><a href="/">Home</a></li>
 						<li>Contacts</li>
 					</ul>
-					<h1>Contacts</h1>	
+					<h1>Contacts</h1>
 				</div>
 			</div>
 		</div>
 	</section>
-<!-- PAGE HEAD END -->
+	<!-- PAGE HEAD END -->
 
-<!-- CONTACTS -->
-<div class="contact-wrap">
-	<div class="container">
-		<div class="row justify-content-md-center">
-			<div class="col-md-6">
-				<div class="contact-left">
-					<h2>Contacts</h2>
-					<div class="item">
-						<div class="title"><i class="fa fa-location-arrow" aria-hidden="true"></i>ADDRESS</div>
-						<p>Desa Mekarwangi, Kecamatan Sindang Kerta, Kabupaten Bandung Barat</p>
-					</div>
-					<div class="item">
-						<div class="title"><i class="fa fa-phone" aria-hidden="true"></i>PHONE</div>
-						<p><a href="https://wa.me/082115265767">+62 821 1526 5767</a></p>
-					</div>
-					<div class="item">
-						<div class="title"><i class="fa fa-envelope-o" aria-hidden="true"></i>EMAIL</div>
-						<p><a href="mailto:mekarwangi.coffee@gmail.com">mekarwangi.coffee@gmail.com</a></p>
-					</div>
-					<div class="item">
-						<div class="title"><i class="fa fa-instagram" aria-hidden="true"></i>INSTAGRAM</div>
-						<p><a href="https://instagram.com/mekarwangi.coffee">mekarwangi.coffee</a></p>
-					</div>
-					<div class="item">
-						<div class="title"><i class="fa fa-facebook" aria-hidden="true"></i>FACEBOOK</div>
-						<p><a href="https://instagram.com/mekarwangi.coffee">Mekar Wangi Coffee</a></p>
+	<!-- CONTACTS -->
+	<div class="contact-wrap">
+		<div class="container">
+			<div class="row justify-content-md-center">
+				<div class="col-md-6">
+					<div class="contact-left">
+						<h2>Contacts</h2>
+						<div class="item">
+							<div class="title"><i class="fa fa-location-arrow" aria-hidden="true"></i>ADDRESS</div>
+							<p>Desa Mekarwangi, Kecamatan Sindang Kerta, Kabupaten Bandung Barat</p>
+						</div>
+						<div class="item">
+							<div class="title"><i class="fa fa-phone" aria-hidden="true"></i>PHONE</div>
+							<p><a href="https://wa.me/082115265767">+62 821 1526 5767</a></p>
+						</div>
+						<div class="item">
+							<div class="title"><i class="fa fa-envelope-o" aria-hidden="true"></i>EMAIL</div>
+							<p><a href="mailto:mekarwangi.coffee@gmail.com">mekarwangi.coffee@gmail.com</a></p>
+						</div>
+						<div class="item">
+							<div class="title"><i class="fa fa-instagram" aria-hidden="true"></i>INSTAGRAM</div>
+							<p><a href="https://instagram.com/mekarwangi.coffee">mekarwangi.coffee</a></p>
+						</div>
+						<div class="item">
+							<div class="title"><i class="fa fa-facebook" aria-hidden="true"></i>FACEBOOK</div>
+							<p><a href="https://instagram.com/mekarwangi.coffee">Mekar Wangi Coffee</a></p>
+						</div>
 					</div>
 				</div>
-			</div>
-			<!-- <div class="col-md-6">
+				<!-- <div class="col-md-6">
 				<div class="contact-right">
 					<h2>Send Message</h2>
 					<form class="contact-form">
@@ -65,14 +65,14 @@
 					</form>
 				</div>
 			</div> -->
+			</div>
 		</div>
 	</div>
-</div>
-<!-- CONTACTS END -->
+	<!-- CONTACTS END -->
 
-<!-- MAP -->
-<div id="map" data-lat="-7.0636314230296495" data-lng="107.3957492711635" data-zoom="11">
-		
-</div>
-<!-- MAP END -->
+	<!-- MAP -->
+	<!-- <div id="map" data-lat="-7.0636314230296495" data-lng="107.3957492711635" data-zoom="11"> -->
+
+	</div>
+	<!-- MAP END -->
 </x-coffee>
